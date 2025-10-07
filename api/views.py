08028a288dd5e86163e94cecc95401b695889a2d
faelcjeronymo <rafaelcjeronymo@outlook.com>
2025-10-06 21:38:05 -0300

    created models viewsets

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Bank, Transaction
+from .serializers import BankSerializer, TransactionSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+
+class BankViewSet(viewsets.ModelViewSet):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
